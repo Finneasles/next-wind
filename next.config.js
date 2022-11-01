1aa20529acc09ccba7f3f5c04ca5b6a76e765ccb
Finneas <85114582+Finneasles@users.noreply.github.com>
2022-11-01 06:52:33 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["flowbite.s3.amazonaws.com"],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
       config.resolve.fallback = {
         fs: false,
       };
     }
-
     return config;
   },
 };
