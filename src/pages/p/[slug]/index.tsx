5c686c2afb874fef59652acfe686d2377f11c21f
Finneas <85114582+Finneasles@users.noreply.github.com>
2022-11-05 05:35:57 +0000

    Create index.tsx

---
+++ src/pages/p/[slug]/index.tsx
@@ -0,0 +1,37 @@
+import { ContentPage } from "@/components";
+import { getFiles, getPostBySlug } from "@/utils";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+
+function Content({ frontMatter, markdownBody }) {
+  return <ContentPage source={{ frontMatter, markdownBody }} />;
+}
+
+export default Content;
+
+export async function getStaticProps({ params }: Params) {
+  const { frontMatter, markdownBody } = await getPostBySlug(
+    params.slug,
+    "posts"
+  );
+
+  return {
+    props: {
+      frontMatter,
+      markdownBody,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getFiles("posts");
+  const paths = posts.map((filename: string) => ({
+    params: {
+      slug: filename.replace(/\.md/, ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
